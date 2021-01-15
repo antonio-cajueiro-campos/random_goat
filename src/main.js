7026320daa8cfd31eb7bef59e240f67986e3e141
antonio-cajueiro-campos <kimeesan5@gmail.com>
2021-01-15 09:12:32 -0300

    1st-release

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
